6b7b0a394d4e75bf13b021d2293d16eb66cf24b5
Andreas Linz <klingt.net@gmail.com>
2014-06-12 20:06:39 +0200

    added some time measurements

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
-from stringmatching import stringmatching
+from stringmatching import stringmatching as sm
 import logging
+import time
+from sys import maxsize
+from pprint import pprint
 
 def main():
     # configure logger
     log_format = '[%(levelname)s] (#%(lineno)s) %(filename)s->%(funcName)s>>> "%(message)s"'
     logging.basicConfig(format=log_format)
-    algs = stringmatching.Algorithms
-    pattern = 'abba'
-    text = 'adaabbaasdf'
-    result = stringmatching.search(pattern, text, algs.naive, all=True)
-    print(result)
-    result = stringmatching.search(pattern, text, algs.last_occ, all=True)
-    print(result)
-    result = stringmatching.search(pattern, text, algs.morris_pratt, all=True)
-    print(result)
-    result = stringmatching.search(pattern, text, algs.knuth_morris_pratt, all=True)
-    print(result)
-    result = stringmatching.search(pattern, text, algs.boyer_moore, all=True)
-    print(result)
+    algs = sm.Algorithms
+    text = None
+    with open('resources/pi_10million.txt', 'r') as f:
+        text = f.read()
+    result = {}
+    for alg in algs:
+        if alg is not algs.boyer_moore:
+            print('Measuring time for "{}" algorithm.'.format(alg.name))
+            result[alg.name] = {
+                'min': maxsize,
+                'max': 0
+            }
+            for i in range(1, 3):
+                start = time.process_time()
+                sm.search('180986', text, alg, True)
+                end = time.process_time()
+                duration = end-start
+                if duration < result[alg.name]['min']:
+                    result[alg.name]['min'] = duration
+                if duration > result[alg.name]['max']:
+                    result[alg.name]['max'] = duration
+    pprint(result)
 
 if __name__ == '__main__':
     main()
