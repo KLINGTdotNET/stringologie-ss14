dfdd297e9621b1fe63a6ef343b237410d2cd22de
Andreas Linz <klingt.net@gmail.com>
2014-07-21 13:52:12 +0200

    changed loopcount and source file

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
         text = f.read()
     result = {}
     pattern = 'Boscombe Valley is a country district not very far from Ross, in Herefordshire. The largest landed proprietor in that part is a Mr. John Turner, who made his money in Australia and returned some years ago to the old country. One of the farms which he held, that of Hatherley, was let to Mr. Charles McCarthy, who was also an ex-Australian. The men had known each other in the colonies, so that it was not unnatural that when they came to settle down they should do so as near each other as possible. Turner was apparently the richer man, so McCarthy became his tenant but still remained'
-    runs = 50
+    runs = 10
     for name in sm.algs:
         alg = sm.algs[name]
         print('Measuring time for "{}" algorithm.'.format(name))
@@ -46,7 +46,7 @@ def main():
         if duration > result['find']['max']:
             result['find']['max'] = duration
     pprint(result)
-    with open('resources/pi_10million.txt', 'r') as f:
+    with open('resources/pi_1million.txt', 'r') as f:
         text = f.read()
     result = {}
     for name in sm.algs:
